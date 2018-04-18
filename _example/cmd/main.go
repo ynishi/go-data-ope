c25d32f64e696ae067ed1d4e9dbb41d62fe28faa
Yutaka Nishimura <ytk.nishimura@gmail.com>
2018-04-18 23:32:59 +0900

    update main.go remove EchoTask

--- _example/cmd/main.go
+++ _example/cmd/main.go
@@ -2,117 +2,29 @@ package main
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
 	"log"
 	"os"
 
 	gdo "github.com/ynishi/go-data-ope"
+	ect "github.com/ynishi/go-data-ope/_example/echotask"
 )
-
-type EchoTask struct {
-	Original string
-	Output   string
-}
-
-type EchoReq struct {
-	Str string
-}
-
-type EchoRes struct {
-	Str string
-}
-
-func (et *EchoTask) Validate(req interface{}) error {
-	if _, ok := req.(EchoReq); !ok {
-		return errors.New("request type is not EchoReq.")
-	}
-	return nil
-}
-
-func (et *EchoTask) Plan(req interface{}, v interface{}) error {
-	echoReq, err := v2EchoReq(req)
-	if err != nil {
-		return err
-	}
-	echoRes, err := v2EchoRes(v)
-	if err != nil {
-		return err
-	}
-	echoRes.Str = echoReq.Str
-	return nil
-}
-
-func (et *EchoTask) Do(req interface{}, v interface{}) error {
-	echoReq, err := v2EchoReq(req)
-	if err != nil {
-		return err
-	}
-	et.Original = et.Output
-	et.Output = echoReq.Str
-	v = echoReq.Str
-	return nil
-}
-func (et *EchoTask) Back(req interface{}, v interface{}) error {
-	echoReq, err := v2EchoReq(req)
-	if err != nil {
-		return err
-	}
-	if echoReq.Str != et.Original {
-		return errors.New("invalid rollback data: original " + et.Original)
-	}
-	et.Original = ""
-	et.Output = et.Original
-	v = et.Output
-	return nil
-}
-func (et *EchoTask) Check(req interface{}) error {
-	echoReq, err := v2EchoReq(req)
-	if err != nil {
-		return err
-	}
-	if echoReq.Str != et.Output {
-		return errors.New("failed to check: " + et.Output)
-	}
-	return nil
-}
-func (et *EchoTask) Monitor(req interface{}, v interface{}) error {
-	return nil
-}
-
-func v2EchoReq(v interface{}) (*EchoReq, error) {
-	var e EchoReq
-	var ok bool
-	if e, ok = v.(EchoReq); !ok {
-		return nil, errors.New("request type is not EchoReq.")
-	}
-	return &e, nil
-}
-
-func v2EchoRes(v interface{}) (*EchoRes, error) {
-	var e *EchoRes
-	var ok bool
-	if e, ok = v.(*EchoRes); !ok {
-		return nil, errors.New("request type is not EchoRes.")
-	}
-	return e, nil
-}
 
 func main() {
 
 	if len(os.Args) != 2 {
 		log.Fatal("payload:$1 is required")
 	}
-	echoReq := EchoReq{os.Args[1]}
+	echoReq := ect.EchoReq{os.Args[1]}
 
 	var task gdo.Tasker
-	task = &EchoTask{}
+	task = &ect.EchoTask{}
 
 	if err := task.Validate(echoReq); err != nil {
 		log.Fatalf("failed to validate: %v\n", err)
 	}
 
-	echoRes := EchoRes{""}
+	echoRes := ect.EchoRes{""}
 	if err := task.Plan(echoReq, &echoRes); err != nil {
 		log.Fatalf("failed to plan: %v\n", err)
 	}
